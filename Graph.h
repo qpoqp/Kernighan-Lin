1f5cdb7bd6d1ed04c662bc0e82651c163d7404b1
qpoqp <37108801+qpoqp@users.noreply.github.com>
2020-08-27 20:34:33 +0300

    Add files via upload

---
+++ Graph.h
@@ -0,0 +1,140 @@
+﻿#pragma once
+#include "cvector.h"
+
+#define first 0x41
+#define second 0x42
+typedef struct Vertex {
+	int index;
+	char subset;
+	float def;
+	float gain;
+} Vertex;
+struct Graph {
+	int V, E;
+	float* Matrix;
+	cvector_vector_type(Vertex*) defs;
+	cvector_vector_type(Vertex*) A;
+	cvector_vector_type(Vertex*) B;
+};
+void test_init(struct Graph* G) {
+	G->defs = NULL;
+	G->A = NULL;
+	G->B = NULL;
+	G->V = 8;
+	G->E = 13;
+	G->Matrix = (float*)malloc(G->V * G->V * sizeof(float));
+	for (int i = 0; i < G->V; i++) {
+		for (int j = 0; j < G->V; j++) {
+			*(G->Matrix + i * G->V + j) = 0;
+		}
+	}
+	*(G->Matrix + 0 * G->V + 1) = 1;
+	*(G->Matrix + 0 * G->V + 4) = 1;
+	*(G->Matrix + 0 * G->V + 5) = 1;
+
+	*(G->Matrix + 1 * G->V + 0) = 1;
+	*(G->Matrix + 1 * G->V + 4) = 1;
+	*(G->Matrix + 1 * G->V + 5) = 1;
+
+	*(G->Matrix + 2 * G->V + 3) = 1;
+	*(G->Matrix + 2 * G->V + 5) = 1;
+	*(G->Matrix + 2 * G->V + 6) = 1;
+	*(G->Matrix + 2 * G->V + 7) = 1;
+
+	*(G->Matrix + 3 * G->V + 2) = 1;
+	*(G->Matrix + 3 * G->V + 6) = 1;
+	*(G->Matrix + 3 * G->V + 7) = 1;
+
+	*(G->Matrix + 4 * G->V + 0) = 1;
+	*(G->Matrix + 4 * G->V + 1) = 1;
+	*(G->Matrix + 4 * G->V + 5) = 1;
+
+	*(G->Matrix + 5 * G->V + 0) = 1;
+	*(G->Matrix + 5 * G->V + 1) = 1;
+	*(G->Matrix + 5 * G->V + 4) = 1;
+	*(G->Matrix + 5 * G->V + 2) = 1;
+
+	*(G->Matrix + 6 * G->V + 2) = 1;
+	*(G->Matrix + 6 * G->V + 3) = 1;
+	*(G->Matrix + 6 * G->V + 7) = 1;
+
+	*(G->Matrix + 7 * G->V + 2) = 1;
+	*(G->Matrix + 7 * G->V + 3) = 1;
+	*(G->Matrix + 7 * G->V + 6) = 1;
+
+
+}
+void graph_init(struct Graph* G, char** argv) {
+	G->defs = NULL;
+	G->A = NULL;
+	G->B = NULL;
+	unsigned char buffer[4];
+	FILE* connection = fopen(argv[1], "rb");
+	for (int i = 0; i < 4; i++)
+		buffer[i] = getc(connection);
+	G->V = *(int*)& buffer;
+	for (int i = 0; i < 4; i++)
+		buffer[i] = getc(connection);
+	G->E = *(int*)& buffer;
+	G->Matrix = (float*)malloc(G->V * G->V * sizeof(float));
+	for (int i = 0; i < G->V; i++) {
+		for (int j = 0; j < G->V; j++) {
+			for (int i = 0; i < 4; i++) {
+				buffer[i] = getc(connection);
+			}
+			*(G->Matrix + i * G->V + j) = *(float*)& buffer;
+		}
+	}
+	fclose(connection);
+}
+void print_graph(struct Graph* G) {
+	float edges_sum = 0;
+	for (int i = 0; i < G->V; i++) {
+		for (int j = 0; j < G->V; j++) {
+			printf("%.2f ", *(G->Matrix + j * G->V + i));
+			edges_sum += *(G->Matrix + j * G->V + i);
+		}	
+		printf("\n");
+	}
+	printf("\nEdges:%d\tsum%.2f\n",G->E, edges_sum);
+	system("pause");
+}
+void random_cut(struct Graph* G, int thread) {//Случайное разделение необходимо для разных потоков
+
+	for (int i = 0; i < G->V; i+=2) {
+		Vertex* v = (Vertex*)malloc(sizeof(Vertex));
+		Vertex* v2 = (Vertex*)malloc(sizeof(Vertex));
+		v->index = i;
+		v2->index = i+1;
+		if (rand() % 100 < 50) {
+			v->subset = first;
+			v2->subset = second;
+		}
+		else {
+			v->subset = second;
+			v2->subset = first;
+		}
+		cvector_push_back(G->defs, v);
+		cvector_push_back(G->defs, v2);
+	}
+	//int half = G->V / 2;
+	//for (int i = 0; i < G->V; i++) {
+	//	Vertex* v = (Vertex*)malloc(sizeof(Vertex));
+	//	v->index = i;
+	//	v->subset = (i < half) ? first : second;
+	//	cvector_push_back(G->defs, v);
+	//}
+	//if (thread < half * half) {
+	//	char t = G->defs[thread / half]->subset;
+	//	G->defs[thread / half]->subset = G->defs[half + thread % half]->subset;
+	//	G->defs[half + thread % half]->subset = t;
+	//}
+}
+void print_defs(struct Graph* G) {
+	printf("Defs:\n");
+	for (int i = 0; i < G->V; i++) {
+		printf("%d\t%f\n", G->defs[i]->index, G->defs[i]->def);
+	}
+	getchar();
+	
+}
---
+++ Source.c
@@ -0,0 +1,124 @@
+﻿//#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "cvector.h"
+#include "Graph.h"
+//a ∈ A и b ∈ B
+//Дефицит вершины = (сумма весов ребер между 'a' и другой вершиной из B) - (сумма весов ребер между 'a' и другой вершиной из A)
+void def_calc(struct Graph* G) {
+	float def = 0;
+	for (int i = 0; i < G->V; i++) {
+		def = 0;
+		for (int j = 0; j < G->V; j++) {
+			float weight = *(G->Matrix + G->defs[i]->index * G->V + G->defs[j]->index);
+			if (weight) {//Если существует ребро между вершинами i j
+				if (G->defs[i]->subset == G->defs[j]->subset)
+					def -= weight;
+				else
+					def += weight;
+			}
+		}
+		G->defs[i]->def = def;
+	}
+}
+void select_pair(struct Graph* G) {
+	int _a = -1; int _b = -1; float m_gain = INT_MIN;
+	for (int i = 0; i < cvector_size(G->defs); i++) {
+		for (int j = 0; j < cvector_size(G->defs); j++) {
+			if (G->defs[i]->subset != G->defs[j]->subset) {//Если вершины из разных подмножеств
+				float c_gain = G->defs[i]->def + G->defs[j]->def - 2 * (*(G->Matrix + G->defs[i]->index * G->V + G->defs[j]->index));//текущая выгода
+				if (c_gain > m_gain) {
+					_a = i;
+					_b = j;
+					m_gain = c_gain;
+				}
+			}
+		}
+	}
+	if (cvector_size(G->A) > 0) {//Если есть предыдущие выгоды
+		m_gain += G->A[cvector_size(G->A) - 1]->gain;
+	}
+	G->defs[_a]->gain = G->defs[_b]->gain = m_gain;
+	if (G->defs[_a]->subset == first) {
+		cvector_push_back(G->A, G->defs[_a]);
+		cvector_push_back(G->B, G->defs[_b]);
+	}
+	else {
+		cvector_push_back(G->A, G->defs[_b]);
+		cvector_push_back(G->B, G->defs[_a]);
+	}
+	cvector_erase(G->defs, _a);
+	if (_a < _b)//Если a находится раньше, b смещается на 1
+		_b--;
+	cvector_erase(G->defs, _b);
+
+	//Пересчет дефицитов
+	for (int i = 0; i < cvector_size(G->defs); i++) {
+		float weight_ia = *(G->Matrix + G->defs[i]->index * G->V + G->A[cvector_size(G->A) - 1]->index);//Вес между i и последней вершиной из  A
+		float weight_ib = *(G->Matrix + G->defs[i]->index * G->V + G->B[cvector_size(G->B) - 1]->index);//Вес между i и последней вершиной из  B
+		if (G->defs[i]->subset == first) {
+			G->defs[i]->def += 2 * weight_ia - 2 * weight_ib;
+		}
+		else {
+			G->defs[i]->def += 2 * weight_ib - 2 * weight_ia;
+		}
+	}
+}
+int main(int argc, char* argv[]) {//1) filename with graph
+	struct Graph G;
+	srand(time(NULL));
+	graph_init(&G, argv);
+	random_cut(&G, 0);
+	float g_max;
+	int k;
+	int count = 0;
+	do {
+		g_max = 0;
+		k = -1;
+		def_calc(&G);
+		for (int i = 0; i < G.V / 2; i++) {
+			select_pair(&G);
+		}
+		for (int i = 0; i < G.V / 2; i++) {
+			if (G.A[i]->gain > g_max) {
+				g_max = G.A[i]->gain;
+				k = i;
+			}
+		}
+		if ((int)g_max > 0) {
+			while (!cvector_empty(G.A)) {
+				if (k >= 0) {
+					G.A[0]->subset = second;
+					G.B[0]->subset = first;
+					k--;
+				}
+				G.A[0]->def = G.B[0]->def = INT_MIN;
+				G.A[0]->gain = G.B[0]->gain = INT_MIN;
+				cvector_push_back(G.defs, G.A[0]);
+				cvector_push_back(G.defs, G.B[0]);
+				cvector_erase(G.A, 0);
+				cvector_erase(G.B, 0);
+			}
+			float a = 0; float b = 0; float deleted = 0;
+			for (int i = 0; i < G.V; i++) {
+				for (int j = 0; j < G.V; j++) {
+					if (G.defs[i]->subset == G.defs[j]->subset)
+						if (G.defs[i]->subset == first)
+							a += *(G.Matrix + G.defs[i]->index * G.V + G.defs[j]->index);
+						else
+							b += *(G.Matrix + G.defs[i]->index * G.V + G.defs[j]->index);
+					else
+						deleted += b += *(G.Matrix + G.defs[i]->index * G.V + G.defs[j]->index);
+				}
+			}
+			printf("\n%d)Gain:%f\tA:%.2f\tB:%.2f\tD:%.2f", count, g_max, a, b, a - b);
+		}
+		
+	} while ((int)g_max > 0);
+	cvector_free(G.defs);
+	G.defs = NULL;
+
+	return 0;
+}
